27989bc52eaa832c9bb2ae5fc43c21bc2bf9668c
ThanakornSaokum <141802771+ThanakornSaokum@users.noreply.github.com>
2024-09-02 16:15:19 +0700

    Update q3.ts

--- q3.ts
+++ q3.ts
@@ -1,9 +1,14 @@
 /* define interface for Runner objects */
-interface Runner {}
+interface Runner {
+  name: string;
+  time: number;
+}
 
 /* assign interface/type to the function definition properly */
 const q3 = (runners: Runner[]): Runner[] => {
-  //your code here
+  const avg = runners.reduce((curr, runner) => runner.time + curr, 0) / runners.length;
+
+  return runners.filter(runner => runner.time <= avg);
 };
 
 //test cases
