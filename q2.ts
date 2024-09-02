5658b0e635c0558d4fdcacb8b6cc24abc7d55eed
ThanakornSaokum <141802771+ThanakornSaokum@users.noreply.github.com>
2024-09-02 16:21:23 +0700

    Update q2.ts

--- q2.ts
+++ q2.ts
@@ -1,6 +1,13 @@
 /* assign interface/type to the function definition properly */
-const q2 = (m, n) => {
+const q2 = (m: number, n: number): number => {
   /* Your code here */
+  let sum : number = 0;
+  for(let i = m; i <= n; i++){
+    if(i % 2 !== 0){
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 //test cases
